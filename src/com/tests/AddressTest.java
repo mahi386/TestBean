8315383742b806d567a1a65836715835cf70133a
Prajakta <pshegde@ncsu.edu>
2013-10-04 16:40:23 -0400

    moved beantester initialization code to beforeclass method

--- src/com/tests/AddressTest.java
+++ src/com/tests/AddressTest.java
@@ -2,21 +2,26 @@ package com.tests;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.beans.Address;
 import com.utilities.BeanTester;
 
 public class AddressTest {
-private BeanTester<Address> beanTester;
+	private static BeanTester<Address> beanTester;
+	
+	@BeforeClass
+	public static void initializeBeanTester() {
+		beanTester = new BeanTester<Address>(Address.class);   
+	}
 	
 	/**
 	 * This test tests if getters are present for all fields in the class and the names are in the proper format
 	 */
 	@Test
 	public void testGettersAddress() {
-		beanTester = new BeanTester<Address>(Address.class);   
-        assertTrue(beanTester.testGetters());
+		assertTrue(beanTester.testGetters());
 	}
 	
 	/**
@@ -24,8 +29,7 @@ private BeanTester<Address> beanTester;
 	 */
 	@Test
 	public void testSettersAddress() {
-		beanTester = new BeanTester<Address>(Address.class);   
-        assertTrue(beanTester.testSetters());
+		assertTrue(beanTester.testSetters());
 	}
 	
 }
--- src/com/tests/PersonTest.java
+++ src/com/tests/PersonTest.java
@@ -2,21 +2,25 @@ package com.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
-
 import com.beans.Person;
 import com.utilities.BeanTester;
 
 public class PersonTest {
-	private BeanTester<Person> beanTester;
+	private static BeanTester<Person> beanTester;
+	
+	@BeforeClass
+	public static void initializeBeanTester() {
+		beanTester = new BeanTester<Person>(Person.class);   
+	}
 	
 	/**
 	 * This test tests if getters are present for all fields in the class and the names are in the proper format
 	 */
 	@Test
 	public void testGettersPerson() {
-		beanTester = new BeanTester<Person>(Person.class);   
-        assertTrue(beanTester.testGetters());
+		assertTrue(beanTester.testGetters());
 	}
 	
 	/**
@@ -24,7 +28,6 @@ public class PersonTest {
 	 */
 	@Test
 	public void testSettersPerson() {
-		beanTester = new BeanTester<Person>(Person.class);   
-        assertTrue(beanTester.testSetters());
+		assertTrue(beanTester.testSetters());
 	}
 }
